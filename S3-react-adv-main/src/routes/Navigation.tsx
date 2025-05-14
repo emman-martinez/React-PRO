9956a852230c78b65114c61e232d829295ceb84e
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-05-14 16:08:33 -0600

    Refactor Navigation component to remove lazy loading and update routes

--- S3-react-adv-main/src/routes/Navigation.tsx
+++ S3-react-adv-main/src/routes/Navigation.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+
 import logo from "../logo.svg";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
 
 export const Navigation = () => {
   return (
@@ -12,37 +12,37 @@ export const Navigation = () => {
           <ul>
             <li>
               <NavLink
-                to="/lazy1"
+                to="/home"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Lazy 1
+                Home
               </NavLink>
             </li>
             <li>
               <NavLink
-                to="/lazy2"
+                to="/about"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Lazy 2
+                About
               </NavLink>
             </li>
             <li>
               <NavLink
-                to="/lazy3"
+                to="/users"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Lazy 3
+                Users
               </NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="lazy1" element={<LazyPage1 />} />
-          <Route path="lazy2" element={<LazyPage2 />} />
-          <Route path="lazy3" element={<LazyPage3 />} />
+          <Route path="about" element={<h1>About Page</h1>} />
+          <Route path="users" element={<h1>Users Page</h1>} />
+          <Route path="home" element={<h1>Home Page</h1>} />
 
-          <Route path="/*" element={<Navigate to="/lazy1" replace />} />
+          <Route path="/*" element={<Navigate to="/home" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
