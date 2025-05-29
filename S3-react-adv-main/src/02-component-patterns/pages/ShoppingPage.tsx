dad3b9d25cc901ca592f0a18b8245943ed323e40
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-05-29 17:53:12 -0600

    Version 0.0.1 ready

--- S3-react-adv-main/src/02-component-patterns/pages/ShoppingPage.tsx
+++ S3-react-adv-main/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,7 +4,6 @@ import ProductCard, {
   ProductTitle,
 } from "../components";
 import { products } from "../data/products";
-import "../styles/custom-styles.css";
 
 const product = products[0];
 
@@ -15,26 +14,14 @@ const ShoppingPage = () => {
       <hr />
       <ProductCard
         key={product.id}
-        className="bg-dark text-white"
-        initialValues={{ count: 6, maxCount: 10 }}
+        initialValues={{ count: 6 /* maxCount: 10 */ }}
         product={product}
       >
         {({ count, maxCount, isMaxCountReached, increaseBy, reset }) => (
           <>
-            <ProductImage
-              className="custom-image"
-              style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
-            />
-            <ProductTitle className=" text-bold" />
-            <ProductButtons className="custom-buttons" />
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(2)}>+2</button>
-            )}
-            <span className="text-white">
-              {count} - {maxCount}
-            </span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
